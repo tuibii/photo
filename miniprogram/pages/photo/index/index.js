953c5423ee334f933a1570d4040d55e26b9253e4
wuyiliang <771134168@qq.com>
2019-07-09 17:49:58 +0800

    Initial Commit

---
+++ miniprogram/pages/photo/index/index.js
@@ -0,0 +1,133 @@
+// pages/photo/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    albumList:[
+      {
+        headPhoto: '../../../images/photo.png',
+        name: 'tt1',
+        time: '2019年7月9日17:15:06',
+        photo: [
+          '../../../images/1.png',
+          '../../../images/1.png',
+          '../../../images/1.png',
+          '../../../images/1.png'
+        ]
+      },
+      {
+        headPhoto: '../../../images/photo.png',
+        name: 'tt2',
+        time: '2019年7月9日17:15:06',
+        photo: [
+          '../../../images/1.png',
+          '../../../images/1.png',
+          '../../../images/1.png',
+          '../../../images/1.png'
+        ]
+      },
+      {
+        headPhoto: '../../../images/photo.png',
+        name: 'tt3',
+        time: '2019年7月9日17:15:06',
+        photo: [
+          '../../../images/1.png',
+          '../../../images/1.png',
+          '../../../images/1.png',
+          '../../../images/1.png'
+        ]
+      },
+      {
+        headPhoto: '../../../images/photo.png',
+        name: 'tt4',
+        time: '2019年7月9日17:15:06',
+        photo: [
+          '../../../images/1.png',
+          '../../../images/1.png',
+          '../../../images/1.png',
+          '../../../images/1.png'
+        ]
+      },
+      {
+        headPhoto: '../../../images/photo.png',
+        name: 'tt5',
+        time: '2019年7月9日17:15:06',
+        photo: [
+          '../../../images/1.png',
+          '../../../images/1.png',
+          '../../../images/1.png',
+          '../../../images/1.png'
+        ]
+      },
+      {
+        headPhoto: '../../../images/photo.png',
+        name: 'tt6',
+        time: '2019年7月9日17:15:06',
+        photo: [
+          '../../../images/1.png',
+          '../../../images/1.png',
+          '../../../images/1.png',
+          '../../../images/1.png'
+        ]
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
